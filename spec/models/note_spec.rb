f442b1d68a9ed03e18b6fc5c76e018e196b9ba89
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-10-05 04:31:39 +0900

    失敗テスト

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  # 検索文字列に一致するメモを返すこと
-  it "returns notes that match the search term" do
+  # 検索結果が1件も見つからなければ空のコレクションを返すこと
+  it "returns an empty collection when no results are found" do
     user = User.create(
       first_name: "Joe",
       last_name:  "Tester",
       email:      "joetester@example.com",
       password:   "dottle-nouveau-pavilion-tights-furze",
     )
+  
 
     project = user.projects.create(
       name: "Test Project",
@@ -27,7 +28,6 @@ RSpec.describe Note, type: :model do
       user: user,
     )
 
-    expect(Note.search("first")).to include(note1, note3)
-    expect(Note.search("first")).to_not include(note2)
+    expect(Note.search("message")).to be_empty
   end
 end
